e73db5ae900bf27eb389ab86a1c9c6c0b1409fc7
dooms90 <ksimran7990@gmail.com>
2026-07-07 22:40:04 +0530

    update in setting

--- Ecommerce_billing_system/settings.py
+++ Ecommerce_billing_system/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'ecommerce_billing',
         'USER':'root',
-        'PASSWORD':'chuja9079',
+        'PASSWORD':'password',
         'HOST':'localhost',
         'PORT':'3306'
     }
